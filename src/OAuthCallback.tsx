2b5f1bc7a61759be3189b335049814c1ac46636f
meloniouss <andrei.jianu31@gmail.com>
2024-11-26 22:33:18 -0500

    another change test

--- src/OAuthCallback.tsx
+++ src/OAuthCallback.tsx
@@ -5,12 +5,12 @@ const OAuthCallback = () => {
     const urlParams = new URLSearchParams(window.location.search);
     const token = urlParams.get('token');
     console.log(token);
-    
+
     if (token) {
       const cookieExpiration = new Date();
       cookieExpiration.setTime(cookieExpiration.getTime() + (24 * 60 * 60 * 1000)); // 1 day
       console.log(cookieExpiration);
-      document.cookie = `sessionToken=${token}; path=/; expires=${cookieExpiration.toUTCString()};`;
+      document.cookie = `sessionToken=${token}; path=/; expires=${cookieExpiration.toUTCString()}; Secure; SameSite=None;`;
       window.location.href = '/otherpage'; 
     } else {
       console.error('No token found in URL');
